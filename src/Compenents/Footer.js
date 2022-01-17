7a8210098d7ef49bb27a7ef9de9745117b30279a
oumaimasb <96146890+oumaimasb@users.noreply.github.com>
2022-01-17 15:26:16 +0100

    footer done

--- src/Compenents/Footer.js
+++ src/Compenents/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../Styles/Footer.css";
+import Logo from "../Compenents/Logo";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <p>We are a team of professional cooks who are excited their food</p>
+      <Logo />
+    </footer>
   );
 };
 
